2779e4ceca1f4fbe91c1d244e5b95cd1520e0c66
Ali <112400141+amajks@users.noreply.github.com>
2024-03-18 21:59:55 +0300

    Delete Changelog directory
    - Was not working
    - Broke the website

--- Changelog/scipt.js
+++
@@ -1,6 +0,0 @@
-// Add toggle functionality to show/hide changes on click
-document.querySelectorAll('.change-item').forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('expanded');
-    });
-});
